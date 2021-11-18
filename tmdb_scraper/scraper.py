16a905b20e63242ec8ef5f118bcdd5252600c716
jaredd <jareddobry10@gmail.com>
2021-11-17 20:56:04 -0500

    Add data scraped and some updates to script

--- tmdb_scraper/scraper.py
+++ tmdb_scraper/scraper.py
@@ -6,6 +6,9 @@ import logging
 import signal
 from multiprocessing import Process, Queue, Event
 from queue import Empty
+from collections import deque
+import time
+from typing import Union
 
 
 NUM_WORKERS = 4
@@ -19,6 +22,7 @@ parser.add_argument(
     "out_file", help="The filename/path to file you want to write to", type=str
 )
 parser.add_argument("api_key", help="Your API key", type=str)
+parser.add_argument("n", help="Number of movies to pull info for", type=int)
 parser.add_argument(
     "--progress_bar", type=bool, default=True, help="Flag to display progress bar"
 )
@@ -49,10 +53,29 @@ def kill_handler(_, __):
     logging.error("kill disabled while writing")
 
 
-def get_movie(movie_id: int) -> dict:
+def get_movie(movie_id: int) -> Union[dict, None]:
     movie = tmdb.Movies(movie_id)
     response = movie.info()
-    return response
+    if response["adult"]:
+        return None
+    return {
+        "belongs_to_collection": response["belongs_to_collection"],
+        "budget": response["budget"],
+        "genres": response["genres"],
+        "id": response["id"],
+        "imdb_id": response["imdb_id"],
+        "overview": response["overview"],
+        "popularity": response["popularity"],
+        "production_companies": response["production_companies"],
+        "production_countries": response["production_countries"],
+        "release_date": response["release_date"],
+        "revenue": response["revenue"],
+        "runtime": response["runtime"],
+        "spoken_languages": response["spoken_languages"],
+        "title": response["title"],
+        "vote_average": response["vote_average"],
+        "vote_count": response["vote_count"],
+    }
 
 
 def worker(work_queue: Queue, output_queue: Queue, kill: Event, idle: Event):
@@ -61,7 +84,8 @@ def worker(work_queue: Queue, output_queue: Queue, kill: Event, idle: Event):
             movie_id = work_queue.get_nowait()
             idle.clear()
             movie = get_movie(movie_id)
-            output_queue.put(movie)
+            if movie:
+                output_queue.put(movie)
         except Empty:
             idle.set()
             continue
@@ -82,6 +106,7 @@ def main():
     logging.info("Json dump loaded")
 
     # Open the output file if it exists to check where we are progress wise
+    count = 0
     if os.path.exists(args.out_file) and os.path.isfile(args.out_file):
         logging.info(f"Reading {args.out_file}")
         processed_ids = []
@@ -94,7 +119,8 @@ def main():
         for movie_id in to_get:
             if movie_id in processed_ids:
                 to_get.remove(movie_id)
-        logging.info("Done processing existing data")
+                count += 1
+        logging.info(f"Done processing existing data. {count} entries existed.")
 
     # Process setup
     logging.info("Spawning sub-processes")
@@ -129,10 +155,12 @@ def main():
     logging.info("Sub-processes started")
 
     # Process output
-    count = 0
+    logging.info(f"Processing {args.n} non-adult movies.")
+    times = deque(maxlen=100000)
+    now = time.time()
     all_idle = False
     try:
-        while not all_idle:
+        while count <= args.n:
             # See if they are idle
             all_idle = True
             for p in processes:
@@ -141,25 +169,46 @@ def main():
                     break
 
             # Try to get stuff to output
-            while True:
+            while count <= args.n:
                 try:
                     movie = output_queue.get_nowait()
                     write_cache.append(movie)
                     count += 1
 
+                    if len(write_cache) > 100:
+                        write_out(args.out_file)
+                        write_cache.clear()
+
                     if args.progress_bar:
+                        new_now = time.time()
+                        times.append(new_now - now)
+                        now = new_now
+
                         os.system("cls" if os.name == "nt" else "clear")
-                        progress = count / num_tasks
+                        progress = count / args.n
                         bars = int(round(50 * progress))
+
+                        avg = sum(times) / len(times)
+                        time_est = avg * (args.n - count)
+                        hours = int(time_est / 3600)
+                        time_est -= hours * 3600
+                        minutes = int(time_est / 60)
+                        time_est -= minutes * 60
+                        seconds = int(time_est)
+
                         progress_str = (
                             "["
                             + bars * "|"
                             + (50 - bars) * " "
-                            + "] {percent:.2f}% - Last movie: {title}"
+                            + "] {percent:.2f}% - Est time remaining: {hours} hours {minutes} minutes {seconds} seconds - Last movie: {title}"
                         )
                         logging.info(
                             progress_str.format(
-                                percent=progress * 100, title=movie["title"]
+                                percent=progress * 100,
+                                title=movie["title"],
+                                hours=hours,
+                                minutes=minutes,
+                                seconds=seconds,
                             )
                         )
                     else:
