b86dea286cf384ebba9ef0e852dffd03e136defa
Hyunjo <hjchoi94@gmail.com>
2021-11-18 23:18:25 -0500

    metric.py notes on important features

--- ml_src/metrics.py
+++ ml_src/metrics.py
@@ -23,5 +23,14 @@ def get_genres(ds: pd.DataFrame) -> List[List[int]]:
     # genre_list is stored in a pd.Series -> list -> dict. Actual genre is dict["name"]
     pass    
     
+# Features of Interest:
+# Popularity is definitely important, seems to be an aggregate of vote count, # of favorited
+# and # of times added to watchlist
+# Revenue to me is more telling than budget, while there are some terrible movies that
+# rake in money, usually the well-watched ones have grossed a lot of revenue.
+# Vote Count is pretty important, because regardless of good or bad votes, a high vote count
+# suggests a lot of views.
+# Production Companies I think is a lowkey important feature, maybe from country to country
+# it varies, but the well-watched movies are made by the same big name production companies.
     
 
