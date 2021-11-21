c72ff548f2ecf5687dd782f34957b6eabad12e06
Jared Dobry <jdobry@grierforensics.com>
2021-11-21 15:32:24 -0500

    Ran formatter, started adding the command-line functional train function to poetry

--- ml_src/main.py
+++ ml_src/main.py
@@ -1,27 +1,82 @@
+import argparse
 from typing import Dict, List
-import utils
-import metrics
+import ml_src.utils as utils
+import ml_src.metrics as metrics
 import pathlib
 import numpy as np
-from utils import Feature
-from model_runner import ModelRunner
+from tmdb_scraper.scraper import load_json
+from ml_src.utils import Feature
+from ml_src.model_runner import ModelRunner
 from sklearn.linear_model import LinearRegression
+import logging
 from sklearn.preprocessing import OneHotEncoder
 
 # import seaborn as sns
-# import pandas as pd
+import pandas as pd
+
 # import matplotlib.pyplot as plt
 
 PARENT_PATH = pathlib.Path("__dir__").parent.resolve()
-DB_PATH = PARENT_PATH/"StaticDB"
-MODEL_PATH = PARENT_PATH/"models"
+DB_PATH = PARENT_PATH / "StaticDB"
+MODEL_PATH = PARENT_PATH / "models"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("training", type=str)
+parser.add_argument("testing", type=str)
+
+KEY_TO_TYPE_DICT = {
+    "belongs_to_collection": bool,
+    "budget": int,
+    "genres": List,  # One hot
+    "id": int,
+    "imdb_id": int,
+    "overview": str,
+    "popularity": float,
+    "production_companies": List,  # One hot?
+    "production_countries": bool,  # Made in USA or elsewhere
+    "release_date": int,
+    "revenue": int,
+    "runtime": int,
+    "spoken_languages": bool,  # One hot?
+    "title": str,
+    "vote_average": float,
+    "vote_count": int,
+}
+
+
+def model_pipeline(training_filepath: str, testing_filepath: str, keys: List[str]):
+    # Load data
+    if pathlib.Path(training_filepath).suffix == ".pickle":
+        training_data = utils.unpickle_df(pathlib.Path(training_filepath))
+    elif pathlib.Path(training_filepath).suffix == ".json":
+        training_data = pd.DataFrame(load_json(training_filepath))
+    else:
+        raise TypeError("Only .pickle and .json files supported")
+
+    if pathlib.Path(testing_filepath).suffix == ".pickle":
+        testing_data = utils.unpickle_df(pathlib.Path(testing_filepath))
+    elif pathlib.Path(testing_filepath).suffix == ".json":
+        testing_data = load_json(testing_filepath)
+    else:
+        raise TypeError("Only .pickle and .json files supported")
+    logging.info("Files loaded successfully")
+
+    # Thin data to the requested keys
+    logging.info(f"Training model using keys: {keys}")
+
+
+def train_model():
+    logging.basicConfig(level=logging.INFO)
+    args = parser.parse_args()
+    model_pipeline(args.training, args.testing)
+
 
 if __name__ == "__main__":
-    train_df = utils.unpickle_df(DB_PATH/"train_movies.pickle")
-    test_df = utils.unpickle_df(DB_PATH/"test_movies.pickle")  
+    train_df = utils.unpickle_df(DB_PATH / "train_movies.pickle")
+    test_df = utils.unpickle_df(DB_PATH / "test_movies.pickle")
 
     # The lines below will give you info about every column
-    print(train_df.info())
+    # print(train_df.info())
     # print(test_df.info())
 
     drop_ls = [
@@ -49,20 +104,26 @@ if __name__ == "__main__":
         Feature(("revenue",), metrics.get_numeric),
         Feature(("runtime",), metrics.get_numeric),
         Feature(("spoken_languages",), metrics.get_num_spoken_languages),
-        Feature(("vote_average", "vote_count",), metrics.get_vote_popularity),
+        Feature(
+            (
+                "vote_average",
+                "vote_count",
+            ),
+            metrics.get_vote_popularity,
+        ),
     )
-    
+
     model_type = LinearRegression(normalize=True)
     model = ModelRunner(model_type, is_grid_search=True)
     model.fit(train_df, feature_tup)
     # model.predict(test_df)
     # model.explain_py(train_df, test_df, 27)
-    
-    # print("Views per day predictions: ", model.predict(test_df))
-    # print("Training score: ", model.get_score(test_df))
-    # model.save(MODEL_PATH/"Hello")
-    # print("Best score:", model.get_best_score())
-    # print("Best params:", model.get_best_params())
+
+    print("Views per day predictions: ", model.predict(test_df))
+    print("Training score: ", model.get_score(test_df))
+    model.save(MODEL_PATH / "Hello")
+    print("Best score:", model.get_best_score())
+    print("Best params:", model.get_best_params())
 
     # print(50*"=")
 
@@ -72,14 +133,11 @@ if __name__ == "__main__":
     # print("Views per day predictions: ", model2.predict(test_df))
     # print("Training score: ", model2.score(test_df))
 
-
     # This line will print all the "genres" column
     # print(train_df["genres"])
-    
+
     # This line will show you all of the different possibilities for the spoken language category
     # print(train_df["spoken_languages"].value_counts())
-    
-
 
     # IGNORE. Gives a heat map of how features correlate together.
     # genres = pd.get_dummies(train_df["genres"], drop_first=True)
@@ -93,8 +151,7 @@ if __name__ == "__main__":
     # bottom, top = ax.get_ylim()
     # ax.set_ylim(bottom + 0.5, top - 0.5)
     # plt.show()
-    
+
     # genres_movie = lambda genre_dict_ls: [genre_dict["name"] for genre_dict in genre_dict_ls]
     # genre_ls = np.array([genres_movie(movie) for movie in train_df.genres], dtype=object).reshape(-1,1)
     # print(OneHotEncoder().fit_transform(genre_ls))
-    
--- ml_src/metrics.py
+++ ml_src/metrics.py
@@ -3,48 +3,80 @@ import pandas as pd
 from collections import namedtuple
 from typing import List, Dict, Tuple
 
-Feature = namedtuple('Feature', ["feature_keys", "handle"])
+Feature = namedtuple("Feature", ["feature_keys", "handle"])
 Category = namedtuple("Category", ["category_vals", "category_column_names"])
+
 
 def get_numeric(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
-    return Category([[float(x) for x in df[names[0]]]], names) 
+    return Category([[float(x) for x in df[names[0]]]], names)
+
 
 def get_belongs_to_collection(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
     return Category([[1 if d else 0 for d in df[names[0]]]], names)
 
+
 def get_genres(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
-    genres = list(np.unique([genre_dict["name"] for movie_genre in df[names[0]] for genre_dict in movie_genre]))
-    
+    genres = list(
+        np.unique(
+            [
+                genre_dict["name"]
+                for movie_genre in df[names[0]]
+                for genre_dict in movie_genre
+            ]
+        )
+    )
+
     def get_row(genre_dict_ls: List[Dict[str, str]]) -> List[int]:
         row = [0] * len(genres)
         for genre_dict in genre_dict_ls:
             index = genres.index(genre_dict["name"])
             row[index] = 1
         return row
-    
-    return Category(np.array([get_row(movie_genre_ls) for movie_genre_ls in df[names[0]]]).T.tolist(), tuple(genres))
-   
+
+    return Category(
+        np.array(
+            [get_row(movie_genre_ls) for movie_genre_ls in df[names[0]]]
+        ).T.tolist(),
+        tuple(genres),
+    )
+
+
 def get_original_language(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
-    return Category([list(map(int, (df["original_language"] == "en").to_list()))], names)
+    return Category(
+        [list(map(int, (df["original_language"] == "en").to_list()))], names
+    )
+
 
 def get_release_year(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
-    return Category([[1949 if date == "null" else int(date.partition("-")[0]) for date in df[names[0]]]], names)
-   
+    return Category(
+        [
+            [
+                1949 if date == "null" else int(date.partition("-")[0])
+                for date in df[names[0]]
+            ]
+        ],
+        names,
+    )
+
+
 def get_num_spoken_languages(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
     return Category([[len(d_ls) for d_ls in df[names[0]]]], names)
-   
+
+
 def get_vote_popularity(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 2
-    return Category([[float(count) * float(avg) for count, avg in zip(df[names[0]], df[names[1]])]], ("vote_popularity",))
+    return Category(
+        [[float(count) * float(avg) for count, avg in zip(df[names[0]], df[names[1]])]],
+        ("vote_popularity",),
+    )
+
 
 # Features of Interest:
 # Production Companies I think is a lowkey important feature, maybe from country to country
 # it varies, but the well-watched movies are made by the same big name production companies.
-    
-
