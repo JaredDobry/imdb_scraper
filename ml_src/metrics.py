6f1ff0173dbb4cd01dc53aee8fd535128347604e
dlerner97 <dlerner97@gmail.com>
2021-11-20 19:39:46 -0500

    Added more metrics solvers to python file

--- ml_src/metrics.py
+++ ml_src/metrics.py
@@ -1,25 +1,17 @@
-from typing import List, Dict, Tuple
 import numpy as np
 import pandas as pd
 from collections import namedtuple
-
-# TODO: @Jared and Hyun, every one of these functions should return a
-# Category[List[List[numeric]], List[str]] -> Since we need one hot encoding, many of these will
-# return two dimensional data collection. Therefore, the get_training_nparray
-# function just assumes that it data will always be 2-dimensional
+from typing import List, Dict, Tuple
 
 Category = namedtuple("Category", ["category_vals", "category_column_names"])
 
-def __get_numeric__(ds: pd.Series) -> List[List[float]]:
-    return [[float(x) for x in ds]]
+def get_numeric(df: pd.DataFrame, names: Tuple[str]) -> Category:
+    assert len(names) == 1
+    return Category([[float(x) for x in df[names[0]]]], names) 
 
 def get_belongs_to_collection(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
     return Category([[1 if d else 0 for d in df[names[0]]]], names)
-
-def get_budget(df: pd.DataFrame, names: Tuple[str]) -> Category:
-    assert len(names) == 1
-    return Category(__get_numeric__(df[names[0]]), names)
 
 def get_genres(df: pd.DataFrame, names: Tuple[str]) -> Category:
     assert len(names) == 1
@@ -33,14 +25,24 @@ def get_genres(df: pd.DataFrame, names: Tuple[str]) -> Category:
         return row
     
     return Category(np.array([get_row(movie_genre_ls) for movie_genre_ls in df[names[0]]]).T.tolist(), tuple(genres))
-    
+   
+def get_original_language(df: pd.DataFrame, names: Tuple[str]) -> Category:
+    assert len(names) == 1
+    return Category([list(map(int, (df["original_language"] == "en").to_list()))], names)
+
+def get_release_year(df: pd.DataFrame, names: Tuple[str]) -> Category:
+    assert len(names) == 1
+    return Category([[1950 if date == "null" else int(date.partition("-")[0]) for date in df[names[0]]]], names)
+   
+def get_num_spoken_languages(df: pd.DataFrame, names: Tuple[str]) -> Category:
+    assert len(names) == 1
+    return Category([[len(d_ls) for d_ls in df[names[0]]]], names)
+   
+def get_vote_popularity(df: pd.DataFrame, names: Tuple[str]) -> Category:
+    assert len(names) == 2
+    return Category([[float(count) * float(avg) for count, avg in zip(df[names[0]], df[names[1]])]], ("vote_popularity",))
+
 # Features of Interest:
-# Popularity is definitely important, seems to be an aggregate of vote count, # of favorited
-# and # of times added to watchlist
-# Revenue to me is more telling than budget, while there are some terrible movies that
-# rake in money, usually the well-watched ones have grossed a lot of revenue.
-# Vote Count is pretty important, because regardless of good or bad votes, a high vote count
-# suggests a lot of views.
 # Production Companies I think is a lowkey important feature, maybe from country to country
 # it varies, but the well-watched movies are made by the same big name production companies.
     
